a56f4c6d2b2244a8742f8b03c7f36ae8b18ce032
web-dev <nathankim137311@gmail.com>
2021-11-24 02:00:28 -0800

    Updates

--- script.js
+++ script.js
@@ -69,8 +69,8 @@ async function getWeatherForecast(city) {
 
     try {
         const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key1}`, {mode: 'cors'});
-        response.header("Access-Control-Allow-Origin", "*");
-        response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        //response.header("Access-Control-Allow-Origin", "*");
+        //response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         const data = await response.json();
         const forecast = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=alerts,minutely&units=imperial&appid=${key1}`)
         const forecastData = await forecast.json();
